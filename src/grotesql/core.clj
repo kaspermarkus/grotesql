827122d8a5de29a76a3d7d5c4af02050dcf373a5
kij <kijmail@gmail.com>
2011-11-25 01:21:23 +0100

    added defnode

--- src/grotesql/core.clj
+++ src/grotesql/core.clj
@@ -1,1 +1,9 @@
-(ns grotesql.core)
+(ns grotesql.core
+	(:use grotesql.functions))
+
+(defmacro defnode
+	"Create a node for the pipeline.
+	Takes a table-function with parameters"
+	[nname func & param] `(def ~nname (partial ~func  ~@param)))
+
+;(defpipe)

621f04681c847e03d3b1956bc534a8af58559339
kij <kijmail@gmail.com>
2011-12-02 09:50:04 +0100

    prematurely defpipe

--- src/grotesql/core.clj
+++ src/grotesql/core.clj
@@ -4,6 +4,16 @@
 (defmacro defnode
 	"Create a node for the pipeline.
 	Takes a table-function with parameters"
-	[nname func & param] `(def ~nname (partial ~func  ~@param)))
+	[nname func & param] 
+	`(def ~nname (partial ~func  ~@param)))
 
-;(defpipe)
+(defmacro defpipe
+	"Take a list of nodes, and return a named function that
+	takes the last table
+
+	As for now, its the same as  create-pipe, 
+	with ?correct? arguments
+	"
+
+	[pname & function_list]
+	`(def ~pname (apply comp (reverse ~@function_list ))))
--- test/grotesql/test/core.clj
+++ test/grotesql/test/core.clj
@@ -47,5 +47,33 @@
 			{:lname "Jesus", :location "Jerusalem"} 
 			{:lname "Bagchakduak", :location "Bali"}))))
 
+(deftest t-multi-column-pipe
+
+	(defnode fake-input1 emulate-input table1)
+	(defnode fake-input2 emulate-input table2)
+
+	(defnode  drop-fname drop-column [:fname])
+	(defnode drop-country drop-column [:country])
+	(defnode drop-city drop-column [:city])
+	(defnode drop-age drop-column [:age])
+	(defnode create-location str-columns :city :country :location ", ")
+
+	(def input2-list [fake-input2 create-location drop-country drop-city])
+	(defpipe input2-pipe input2-list)
+
+; create primary pipe
+	(def input1-list [ fake-input1 drop-fname (partial concat-tables (input2-pipe)) drop-age])
+	(defpipe input1-pipe input1-list)
+
+	(is (= (input1-pipe) 
+			'({:lname "bbbbbbbbbb", :location "Odense, Denmark"} 
+			{:lname "yyyyyyyyyyyyyy", :location "Geneva, Switzerland"} 
+			{:lname "Markus", :location "Geneva"} 
+			{:lname "Jørgensen", :location "Copenhagen"} 
+			{:lname "Niclas", :location "North Pole"} 
+			{:lname "Jesus", :location "Jerusalem"} 
+			{:lname "Bagchakduak", :location "Bali"}))))
+
+
 
 

4bc451d821f8956bdf760933951a3042055e2488
kij <kijmail@gmail.com>
2011-12-02 19:09:11 +0100

    cleanup

--- project.clj
+++ project.clj
@@ -1,6 +1,3 @@
 (defproject grotesql "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"] [org.clojure/data.csv "0.1.0"] ]
-  :dev-dependencies 
-  [[korma "0.3.0-alpha3"]
-  [postgresql "9.1-901.jdbc4"] ])
+  :dependencies [[org.clojure/clojure "1.3.0"] [org.clojure/data.csv "0.1.0"] ])

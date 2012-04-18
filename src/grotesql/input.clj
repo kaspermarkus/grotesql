60dd6e9a28b66bdd76962ccee5280196b90d44a3
kij <kijmail@gmail.com>
2012-04-18 21:44:19 +0200

    move back to input

--- src/grotesql/input.clj
+++ src/grotesql/input.clj
@@ -3,18 +3,19 @@
          '[clojure.java.io :as io])
 
 (defn input-csv 
-	"Reads csv file, separated by separator. If header is true, first line is considered header"
-	([ filename separator header? ]
-	(let [  full-input (with-open [in-file (io/reader filename)] (doall (csv/read-csv in-file :separator separator)))
-		data (if header? (rest full-input) full-input)
-		headers (if header?
-                        ;read first line of file and change values to keywords in a list
-                        (map (fn [val] (keyword (clojure.string/trim val))) (first full-input)) 
-                        ;else make up our own headers
-                        (map (fn [ num ] (keyword (str "header" num))) (iterate inc 1)))]
-                 (filter identity (map (fn [row] ;filter to ensure no nil values
-			(if (not (and (= [""] row) (= (count row) 1))) ;ignore empty lines (make nil)
-				(apply hash-map (interleave headers row)))) data))))
-	; Limit number of lines taken by supplying it as extra parameter
-	([filename separator header? numlines]
-	(take numlines (input-csv filename separator header?))))
+  "Reads csv file, separated by separator. If header is true, first line is considered header"
+  ([ filename separator header? ]
+     (let [full-input (with-open [in-file (io/reader filename)] 
+                        (doall (csv/read-csv in-file :separator separator)))
+           data (if header? (rest full-input) full-input)
+           headers (if header?
+                                        ;read first line of file and change values to keywords in a listening
+                     (map (fn [val] (keyword (clojure.string/trim val))) (first full-input)) 
+                                        ;else make up our own headers
+                     (map (fn [ num ] (keyword (str "header" num))) (iterate inc 1)))]
+       (filter identity (map (fn [row] ;filter to ensure no nil values
+                               (if (not (and (= [""] row) (= (count row) 1))) ;ignore empty lines (make nil)
+                                 (apply hash-map (interleave headers row)))) data))))
+                                        ; Limit number of lines taken by supplying it as extra parameter
+  ([filename separator header? numlines]
+     (take numlines (input-csv filename separator header?))))

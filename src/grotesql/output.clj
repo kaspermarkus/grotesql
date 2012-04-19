4f7c13729f2e364e5eec6f06f31168eaf227509f
Kasper Markus <kasper2@markus.dk>
2012-04-20 01:04:33 +0200

    Fixed indentation in output.clj file

--- src/grotesql/output.clj
+++ src/grotesql/output.clj
@@ -1,27 +1,27 @@
 (ns grotesql.output)
 (require '[clojure.data.csv :as csv]
-         '[clojure.java.io :as io])
+    '[clojure.java.io :as io])
 
 (defn stdout
-	"Prints table to standard output. Values are comma separated and rows are newline separated.
+    "Prints table to standard output. Values are comma separated and rows are newline separated.
 
-	Arguments:
-	table: the data to print"
-	[ table ]
-	(do
-	   (println (apply str (interpose ", " (map name (keys (first table))))))
-	   (println (apply str (interpose "\n" (map (fn [row] (apply str (interpose ", " (vals row)))) table))))))
+    Arguments:
+    table: the data to print"
+    [ table ]
+    (do
+        (println (apply str (interpose ", " (map name (keys (first table))))))
+        (println (apply str (interpose "\n" (map (fn [row] (apply str (interpose ", " (vals row)))) table))))))
 
 (defn output-csv
-	"Writes to .csv file (comma separated values)
+    "Writes to .csv file (comma separated values)
 
-	filename: the file to write (string)
-	separator: the separator of the csv file (char)
-	header?: true if the first line of the .csv file should be a headers (boolean)"
-	[ filename separator header? table ]
-	(let [  data-body (map (fn [row] (vec (vals row))) table) 
-		out-data (if header? 
-			(cons (vec (map name (keys (first table)))) data-body) 
-			data-body)]
-		(with-open [out-file (io/writer filename)] (csv/write-csv out-file out-data :separator separator))))
-		
+    filename: the file to write (string)
+    separator: the separator of the csv file (char)
+    header?: true if the first line of the .csv file should be a headers (boolean)"
+    [ filename separator header? table ]
+    (let 
+        [ data-body (map (fn [row] (vec (vals row))) table) 
+          out-data (if header? 
+            (cons (vec (map name (keys (first table)))) data-body) 
+            data-body)]
+        (with-open [out-file (io/writer filename)] (csv/write-csv out-file out-data :separator separator))))
